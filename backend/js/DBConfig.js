10a95204b55a91e4ccbc82b9edaadc9b1a612d6b
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-02-13 16:21:02 -0800

    Refactor to use DB when created

--- backend/js/DBConfig.js
+++ backend/js/DBConfig.js
@@ -2,42 +2,39 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 class DBConfig {
-    static config = {
+    static config = mysql.createPool({
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-    };
+        database: process.env.DB_NAME,
+    });
 
-    static createDB = `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`;
-    static createTable = `CREATE TABLE IF NOT EXISTS ${process.env.DB_NAME}.${process.env.DB_TABLE}
-        (patientid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), dateOfBirth DATETIME)`;
+    static async initializeDatabase() {
+        // Create DB if it doesn't exist
+        await this.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
 
-    #database;
+        // Switch to the database
+        await this.query(`USE ${process.env.DB_NAME}`);
 
-    constructor() {
-        this.connectToDB();
+        // Create table if it doesn't exist
+        await this.query(`
+      CREATE TABLE IF NOT EXISTS ${process.env.DB_TABLE} (
+        patientid INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100),
+        dateOfBirth DATETIME
+      )
+    `);
     }
 
-    async connectToDB() {
-        // Create database if it doesn't exist
-        this.#database = mysql.createConnection(DBConfig.config);
-        await this.queryDB(DBConfig.createDB);
-        await this.queryDB(`USE ${process.env.DB_NAME}`);
-        await this.queryDB(DBConfig.createTable);
-    }
-
-    async queryDB(query, params = []) {
+    static query(sql, params = []) {
         return new Promise((resolve, reject) => {
-            this.#database.query(query, params, (err, result) => {
-                if (err) return reject(err);
-                resolve(result);
+            this.config.query(sql, params, (err, result) => {
+                if (err) reject(err);
+                else resolve(result);
             });
         });
     }
-
-    getDatabase() {
-        return this.#database;
-    }
 }
 
-exports.DBConfig = DBConfig;
+module.exports = DBConfig;
