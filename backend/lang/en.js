a2861bd096a2c6a918975c6f453d0874021099ba
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-02-15 20:52:52 -0800

    Updated messages

--- backend/lang/en.js
+++ backend/lang/en.js
@@ -1,4 +1,9 @@
+// This code was assisted by ChatGPT, OpenAI.
 module.exports = {
+    errorNoQuery: "No query provided",
+    errorGet: "400: GET request only supports SELECT queries",
+    errorPost: "POST request only supports INSERT queries",
     error404: "404: Page Not Found",
     error405: "405: Method not supported",
+    success: "Database successfully updated",
 };
