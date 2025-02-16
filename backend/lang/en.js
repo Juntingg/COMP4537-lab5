a438200df3b80e67a514b3212ac4135898a325bf
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-02-16 05:50:50 -0800

    Add 400

--- backend/lang/en.js
+++ backend/lang/en.js
@@ -2,7 +2,7 @@
 module.exports = {
     errorNoQuery: "No query provided",
     errorGet: "400: GET request only supports SELECT queries",
-    errorPost: "POST request only supports INSERT queries",
+    errorPost: "400: POST request only supports INSERT queries",
     error404: "404: Page Not Found",
     error405: "405: Method not supported",
     success: "Database successfully updated",
