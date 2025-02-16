3854a0b9dce11de72f1d3af5295a0598458e26be
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-02-16 03:36:11 -0800

    Add port

--- backend/js/DBManager.js
+++ backend/js/DBManager.js
@@ -6,7 +6,8 @@ class DBManager {
     static config = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
     };
 
     #database;
